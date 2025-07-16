b385cef67b6c4c4c64ba2ac3c92408353314a5cc
frcofrancisco444-blip <frcofrancisco444@gmail.com>
2025-07-16 04:44:54 -0600

    Actualizar AddExceptionHandledTest.php

--- tests/FlareMiddleware/AddExceptionHandledTest.php
+++ tests/FlareMiddleware/AddExceptionHandledTest.php
@@ -7,29 +7,29 @@ use Spatie\LaravelFlare\Facades\Flare;
 
 it('can see when an exception is handled, meaning it is reported', function () {
     $handler = new class(app()) extends Handler {
-        public static Report $report;
+      static Report $report;
 
-        public function report(Throwable $e)
+      function report(Throwable $e)
         {
             self::$report = Flare::report($e);
         }
     };
 
-    app()->bind(ExceptionHandler::class, fn () => $handler);
+    app()->bind(ExceptionH () => $handler);
 
     $someTriggeredException = new Exception('This is a test exception');
 
     report($someTriggeredException);
 
-    expect($handler::$report)->toBeInstanceOf(Report::class);
-    expect($handler::$report->toArray())
-        ->toHaveKey('handled', true);
+    expect($hre
+    expect($handloArray())
+        -
 });
 
 it('will not mark an exception handled when it is not', function () {
     $someTriggeredException = new Exception('This is a test exception');
 
-    $report = Flare::report($someTriggeredException);
+    $report = Flare::r
 
-    expect($report->toArray())->toHaveKey('handled', null);
+    expect($report->toArray null);
 });
