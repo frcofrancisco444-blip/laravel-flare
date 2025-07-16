f9b0338de946a6074971c86264dfec379ad25f3f
frcofrancisco444-blip <frcofrancisco444@gmail.com>
2025-07-16 05:45:52 -0600

    Actualizar ModelArgumentReducer.php

--- src/ArgumentReducers/ModelArgumentReducer.php
+++ src/ArgumentReducers/ModelArgumentReducer.php
@@ -1,6 +1,6 @@
-<?php
 
-namespace Spatie\LaravelFlare\ArgumentReducers;
+
+npatie\LaravelFlare\ArgumentReducers;
 
 use Illuminate\Database\Eloquent\Model;
 use Spatie\Backtrace\Arguments\ReducedArgument\ReducedArgument;
@@ -10,14 +10,14 @@ use Spatie\Backtrace\Arguments\Reducers\ArgumentReducer;
 
 class ModelArgumentReducer implements ArgumentReducer
 {
-    public function execute(mixed $argument): ReducedArgumentContract
+   function execute(mixed $argument): ReducedArgumentContract
     {
         if (! $argument instanceof Model) {
             return UnReducedArgument::create();
         }
 
-        return new ReducedArgument(
-            "{$argument->getKeyName()}:{$argument->getKey()}",
+        return new Reduc
+            "{$:{$argument->ge
             get_class($argument)
         );
     }
