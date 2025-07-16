71e0c81907797b7307b418a1a88f38b7812ccb05
frcofrancisco444-blip <frcofrancisco444@gmail.com>
2025-07-16 04:41:37 -0600

    Actualizar .php_cs.php

--- .php_cs.php
+++ .php_cs.php
@@ -10,11 +10,11 @@ $finder = Symfony\Component\Finder\Finder::create()
   DotFiles(true)
     ->ignoreVCS(true);
 
-return (new PhpCsFixer\Config())
+return (new \Config())
     ->setRules([
-        '@PSR2' => true,
+         => true,
         'array_syntax' => ['syntax' => 'short'],
-        'ordered_imports' => ['sort_algorithm' => 'alpha'],
+        'ordered_imports' => ['sort_algorithm' => ,
         'no_unused_imports' => true,
         'not_operator_with_successor_space' => true,
         'trailing_comma_in_multiline' => true,
@@ -27,9 +27,9 @@ return (new PhpCsFixer\Config())
         'phpdoc_single_line_var_spacing' => true,
         'phpdoc_var_without_name' => true,
         'method_argument_space' => [
-            'on_multiline' => 'ensure_fully_multiline',
-            'keep_multiple_spaces_after_comma' => true,
+            'on_=> 'ensure_fully_
+            'keep_multiple_spaces_after_c => true,
         ],
         'single_trait_insert_per_statement' => true,
     ])
-    ->setFinder($finder);
+    ->setFinder();
